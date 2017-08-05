e8f5dc892af7fa16fdc454e1c7a6aca55ee3f6ce
Manuel Alejandro Cabrera Castillo <manuel.alejandro.cc@gmail.com>
2017-08-05 13:52:48 -0400

    Initial commit

---
+++ website/Models/IdentityModels.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+
+namespace website.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class FantasyUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<FantasyUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public string FullName { get; set; }
+
+        public int Points { get; set; }
+
+        public float Cash { get; set; }
+
+        public virtual ICollection<FantasyUser> Friends { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<FantasyUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
