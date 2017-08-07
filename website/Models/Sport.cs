8a997944336602390556ee47bcafc5701e88a479
Dalia Diaz Sistachs <dsistachs@matcom.uh.cu>
2017-08-07 14:53:33 -0400

    [website] Adding Sport Model

---
+++ website/Models/Sport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace website.Models
+{
+    public class Sport
+    {
+        [Required]
+        public int Id { get; set; }
+        [Display(Name="Sport Name")]
+        public string SportName { get; set; }
+    }
+}
