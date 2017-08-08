64c31c56c7d806761d00722d016b696e851d123b
Dalia Diaz Sistachs <dsistachs@matcom.uh.cu>
2017-08-07 21:37:26 -0400

    [website] Adding Action, ContestType, News, Notification, PromoCode Models

---
+++ website/Models/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace website.Models
+{
+    public class Notification
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required, Display(Name="Notification Name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Notification Content")]
+        public string Content { get; set; }
+    }
+}
