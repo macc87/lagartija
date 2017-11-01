52db2b6eb6f30896e79b924a1b051cff57cd8188
Ivan Hernandez <ihferrero@hotmail.com>
2017-10-31 23:17:17 -0400

    Adding Dtos layer

---
+++ Service.API/Dtos/Fantasy.API.Dtos/Fantasy.API.Dtos/Response/FantasyData/InjuryDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Fantasy.API.Dtos.Response.FantasyData
+{
+    class InjuryDto
+    {
+        public LeagueDto League { get; set; }
+        public IEnumerable<InjuriesTeamDto> teams { get; set; }
+        public string _comment { get; set; }
+    }
+
+    public class InjuriesTeamDto
+    {
+
+    }
+}
