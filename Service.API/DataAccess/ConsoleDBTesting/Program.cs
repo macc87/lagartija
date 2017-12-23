d4b6d6bbe585ce9f5c79569e902e6d9775f05329
Dalia Diaz Sistachs <dsistachs@matcom.uh.cu>
2017-12-23 12:44:45 -0500

    [DataAccess.ConsoleDBTesting] Adding ConsoleDBTesting

---
+++ Service.API/DataAccess/ConsoleDBTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using Fantasy.API.DataAccess.DbContexts.MSSQL.FantasyData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleDBTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FantasyContext dbContest = new FantasyContext();
+
+        }
+}
+}
