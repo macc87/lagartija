9788d4e40ea8559376d7fd60330bd2e8426db99e
Dalia Diaz Sistachs <dsistachs@matcom.uh.cu>
2018-01-04 03:31:41 -0500

    [DataAccess.ConsoleDBTesting] Testing getGames from contest

--- Service.API/DataAccess/ConsoleDBTesting/Program.cs
+++ Service.API/DataAccess/ConsoleDBTesting/Program.cs
@@ -14,13 +14,25 @@ namespace ConsoleDBTesting
     {
         static void Main(string[] args)
         {
+
             FantasyContext dbContext = new FantasyContext();
+            //var gms = dbContext.Games.ToList();
             ContestResponse result = new ContestResponse()
             {
-                Contests = dbContext.Contests.Include("ContestType").ToList()
+                Contests = dbContext.Contests.Include("ContestType").Include("ContestGame").ToList()
             };
+            foreach (Contest c in result.Contests)
+            {
+                c.Games = new List<Game>();
+                foreach (ContestGame cg in c.ContestGame)
+                {
+                    Game g = dbContext.Games.Include("Venue").Include("AwayTeam").Include("HomeTeam").Include("ClimaCondition").First(x => x.GameId == cg.GameId);
+                    //
+                    c.Games.Add(g);
+                }
+            }
             Console.WriteLine("The end");
             Console.ReadLine();
         }
-}
+    }
 }
