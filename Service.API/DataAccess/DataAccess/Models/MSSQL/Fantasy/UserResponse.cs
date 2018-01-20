7bf6027c9880c51a98a410c810af3f87ec9c8436
Dalia Diaz Sistachs <dsistachs@matcom.uh.cu>
2018-01-20 01:17:07 -0500

    [DataAccess] Adding User and Promotions Response

---
+++ Service.API/DataAccess/DataAccess/Models/MSSQL/Fantasy/UserResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy.API.DataAccess.Models.MSSQL.Fantasy
+{
+    public class UserResponse
+    {
+        public Account User { get; set; }
+        public double Money { get; set; }
+        public double Point { get; set; }
+        public List<Account> Friends { get; set; }
+
+        public string _comment { get; set; }
+    }
+}
