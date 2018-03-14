8fab7373839139c7eb3c3d617e000f7e3cd3fb7e
Dalia Diaz Sistachs <dsistachs@gmail.com>
2018-03-14 17:19:03 -0400

    [DataAccess] Adding ClimaConditionResponse, PositionResponse and SportResponse

---
+++ Service.API/DataAccess/DataAccess/Models/MSSQL/Fantasy/SportResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy.API.DataAccess.Models.MSSQL.Fantasy
+{
+    public class SportResponse
+    {
+        public Sport Sport { get; set; }
+        public string _comment { get; set; }
+    }
+}
