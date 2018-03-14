6c489c8056acf3dd3d1b53cde56cf93595d4bd74
Dalia Diaz Sistachs <dsistachs@gmail.com>
2018-03-14 18:44:57 -0400

    [DataAccess] Adding Goal, League, Notification, SingleNews, Venue Responses

---
+++ Service.API/DataAccess/DataAccess/Models/MSSQL/Fantasy/LeagueResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy.API.DataAccess.Models.MSSQL.Fantasy
+{
+    public class LeagueResponse
+    {
+        public League League { get; set; }
+        public string _comment { get; set; }
+    }
+}
