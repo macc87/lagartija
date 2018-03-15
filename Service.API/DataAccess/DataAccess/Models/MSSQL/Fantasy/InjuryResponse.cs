e0c34703761be0b464ff574bf473904351b65f43
Dalia Diaz Sistachs <dsistachs@gmail.com>
2018-03-14 21:25:23 -0400

    [DataAccess] Adding Injuries y LineupResponse

---
+++ Service.API/DataAccess/DataAccess/Models/MSSQL/Fantasy/InjuryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy.API.DataAccess.Models.MSSQL.Fantasy
+{
+    public class InjuryResponse
+    {
+        public Injury Injury { get; set; }
+        public string _comment { get; set; }
+    }
+}
